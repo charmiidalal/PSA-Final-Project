4f41e6393a9fb2a1052c006dabe6d16b3ce5bc2b
apeksha-khandelwal <khandelwal.ap@northeastern.edu>
2021-04-20 10:35:33 -0400

    deleting old UI

--- src/utill/Main.java
+++ src/utill/Main.java
@@ -1,53 +1,9 @@
 package utill;
 
-import Residents.*;
-import org.ini4j.Ini;
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        initGraph();
-        initInfected();
-    }
 
-    private static void initGraph() throws IOException {
-        Ini ini = new Ini(new File("./src/config.ini"));
-        Map<String, String> map = ini.get("default");
-        JFrame frame = new JFrame();
-        JPanel ui = new JPanel();
-        ui.setLayout(new GridLayout(1,3));
-        Simulator graph = new Simulator();
-        Thread panelThread = new Thread(graph);
-        ui.add(graph);
-        ui.add(new Charts());
-        ui.setBackground(Color.BLACK);
-        frame.add(ui);
-        frame.setVisible(true);
-        frame.setTitle(map.get("virus_simulation")+" of "+map.get("city_population")+" Residents of Boston");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1200, 600);
-        frame.setResizable(false);
-        frame.setLocation(50, 50);
-        panelThread.start();
     }
-
-    private static void initInfected() throws IOException {
-        Ini ini = new Ini(new File("./src/config.ini"));
-        Map<String, String> virusParams = ini.get("default");
-        List<Resident> residentList = ResidentDirectory.getInstance().getResidentList();
-        for (int i = 0; i < Integer.parseInt(virusParams.get("randomly_infected")); i++) {
-            Resident resident;
-            do {
-                resident = residentList.get(new Random().nextInt(residentList.size() - 1));
-            } while (resident.isInfected());
-            resident.infectResident();
-        }
-    }
-
 }
